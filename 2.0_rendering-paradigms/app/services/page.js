de23d7d69890ba5ab4c2ee6f5e999890f0630c6b
Shivam Kumar <shivam201907@gmail.com>
2025-03-30 22:15:10 +0530

    Rendering Paradigm in Next.js

---
+++ 2.0_rendering-paradigms/app/services/page.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function Services() {
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link active">
+              Services
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Our Services</h1>
+        <ul className="services-list">
+          <li>Web Development</li>
+          <li>Mobile App Development</li>
+          <li>Consulting Services</li>
+          <li>Digital Marketing</li>
+        </ul>
+      </div>
+    </>
+  );
+}
