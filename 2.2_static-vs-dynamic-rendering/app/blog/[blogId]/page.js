0f4e4c18fb965312b3c316b7796cdea1cb414b62
Shivam Kumar <shivam201907@gmail.com>
2025-04-11 14:22:45 +0530

    Static & Dynamic Rendering

--- 2.2_static-vs-dynamic-rendering/app/blog/[blogId]/page.js
+++ 2.2_static-vs-dynamic-rendering/app/blog/[blogId]/page.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default async function Blog({ params }) {
   const { blogId } = await params;
-  console.log(blogId);
+  console.log("blogId :", blogId);
   return (
-    <div className="flex flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">
         Blogs Page <span className="text-cyan-400">{blogId}</span>
       </h1>
 
       <p>write any blog Name in URL.</p>
-      <p>This is Dynamic Routing</p>
+      <p>This is Dynamic Rendering Page</p>
 
       <Link href="/">
-        <button>Home Page</button>
+        <button className="nav-link active cursor-pointer">Home Page</button>
       </Link>
     </div>
   );
