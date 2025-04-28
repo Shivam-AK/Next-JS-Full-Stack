bd82e1306d93fc61ff6ce429bdb2cdb8a82ad263
Shivam Kumar <shivam201907@gmail.com>
2025-04-28 20:06:21 +0530

    Delete Route Handler

---
+++ 6.6_delete-route-handler/src/app/todos/route.js
@@ -0,0 +1,23 @@
+import todos from "@/app/todos";
+import { writeFile } from "node:fs/promises";
+
+export function GET() {
+  return Response.json(todos);
+}
+
+export async function POST(request) {
+  const todo = await request.json();
+  const newTodo = {
+    id: crypto.randomUUID(),
+    text: todo.text,
+    completed: false,
+  };
+
+  todos.push(newTodo);
+
+  await writeFile("./src/app/todos.json", JSON.stringify(todos, null, 2));
+
+  return Response.json(newTodo, {
+    status: 201,
+  });
+}
